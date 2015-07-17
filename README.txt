d0ca3a184ba4d8dc645948c78df85477e61d003c
hahahahaman <hahahadude@gmail.com>
2015-07-16 21:54:24 -0400

    tidy up a bit

--- README.txt
+++ README.txt
@@ -1,1 +1,7 @@
-Experiments in entity-component-system.
+qua - I wanted to name it quanta, but qua is short and sweet.
+
+Experimental entity-component-system.
+
+Load with asdf or quicklisp or something.
+
+Very simple example provided.
--- examples/examples.lisp
+++ examples/examples.lisp
@@ -5,14 +5,6 @@
 
 (defsystem pos-system (pos))
 
-#|
-(defmethod update-system ((world qua:world) (system pos-system) dt)
-  (iter (for (entity-id n) in-hashtable (entities system))
-    (let ((pos (gethash 'pos (components world entity-id))))
-      (format t "x: ~a, y: ~a, z: ~a ~%" (x pos) (y pos) (z pos))
-      ;; (format t "~a ~%" pos)
-      )))
-|#
 (defmethod update-system ((world qua:world) (system pos-system) dt)
   (with-components (pos) world system
     (format t "~a, ~a, ~a~%" (x pos) (y pos) (z pos))))
@@ -37,27 +29,6 @@
     (print-table (components w e))
     (add-system w pos-sys)
     (add-system w vel-sys)
-    (remove-component w e vel)
     (add-component w e vel)
     (initialize-systems w)
-    (iter (for i from 1 to 10) (update w 0.5))
-  ))
-;; (defun test-interface ()
-;;   (defcomponent point ()
-;;     (x y z))
-
-;;   (defcomponent velocity (point)
-;;     (vx vy vz))
-
-;;   (defsystem point (entity point)
-;;     (format t "entity ~a at pos (~a, ~a, ~a)~%" entity (x point) (y point) (z point)))
-
-;;   (defsystem velocity (entity velocity point)
-;;     (incf (x point) (vx velocity))
-;;     (incf (y point) (vy velocity))
-;;     (incf (z point) (vz velocity)))
-
-;;   (make-entity nil '(point) :x 1 :y 2 :z 3)
-;;   (make-entity nil '(point velocity) :x 4 :y 5 :z 6 :vx -1 :vy -2 :vz -3)
-
-;;   (loop repeat 10 do (system-loop)))
+    (iter (for i from 1 to 10) (update w 0.5))))
